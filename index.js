93fb3d9377906d390455c504c81d435239fd92f5
Sienna <remicknsisi@gmail.com>
2022-10-24 14:34:56 -0400

    commit tree bc219d9ed9ee19b935650e14fb7b05c2b1ec2024

--- index.js
+++ index.js
@@ -1,5 +1,78 @@
 /* Your Code Here */
+function createEmployeeRecord (employee) {
+    let employeeRecord = {
+        'firstName': `${employee[0]}`,
+        'familyName': `${employee[1]}`,
+        'title': `${employee[2]}`,
+        'payPerHour': employee[3],
+        'timeInEvents': [],
+        'timeOutEvents': [],
+    }
 
+    return employeeRecord
+}
+
+function createEmployeeRecords (employees) {
+    let employeeRecords = employees.map(employee => createEmployeeRecord.call(this, employee))
+
+    return employeeRecords
+}
+
+function createTimeInEvent (dateStamp) {
+    let date = dateStamp.split(' ')[0]
+    let hour = dateStamp.split(' ')[1]*1
+    
+    let timeInEvent = {
+        'type': "TimeIn",
+        'hour': hour,
+        'date': `${date}`,
+    }
+
+    this.timeInEvents.push(timeInEvent)
+
+    return this
+}
+
+function createTimeOutEvent (dateStamp) {
+    let date = dateStamp.split(' ')[0]
+    let hour = dateStamp.split(' ')[1]*1
+    
+    let timeOutEvent = {
+        'type': "TimeOut",
+        'hour': hour,
+        'date': `${date}`,
+    }
+
+    this.timeOutEvents.push(timeOutEvent)
+
+    return this
+}
+
+function hoursWorkedOnDate (date) {
+    let timeInEventOnDate = this.timeInEvents.find(timeInEvent => timeInEvent.date === date)
+    let timeInOnDate = timeInEventOnDate.hour
+    
+    let timeOutEventOnDate = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date)
+    let timeOutOnDate = timeOutEventOnDate.hour
+
+    let hoursWorked = (timeOutOnDate - timeInOnDate)/100
+
+    return hoursWorked
+}
+
+function wagesEarnedOnDate (date) {
+    let payRate = this.payPerHour
+
+    let hoursOnDate = hoursWorkedOnDate.call(this, date)
+
+    let payOwed = payRate*hoursOnDate
+
+    return payOwed
+}
+
+function findEmployeeByFirstName (srcArray, first_Name) {
+    return srcArray.find(record => record.firstName === first_Name)
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -10,6 +83,7 @@
  */
 
 const allWagesFor = function () {
+    console.log(this)
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -21,3 +95,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll(employeeRecords){
+    let totalPay = employeeRecords.map(employeeRecord => allWagesFor.call(employeeRecord))
+    
+    return totalPay.reduce(function(a, eachPay){
+        return a + eachPay
+    }, 0)
+}
